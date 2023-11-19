69369a462cf3493b6cdb84ecd893dc3c02c1c570
s.amirhossein <63861146+s-amir@users.noreply.github.com>
2023-11-19 10:28:58 +0330

    Delete blu1 directory

--- blu1/initialize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p data config_kafka data_kafka zookeeper/conf zookeeper/data zookeeper/logs kafka_logs kafka_zookeeper
-
-chown -R 1001:1001 ./zookeeper config_kafka/ data_kafka/ kafka_logs/ data kafka_zookeeper
